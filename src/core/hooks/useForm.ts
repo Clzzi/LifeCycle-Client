277428990f8d39d33e877815a0a61bf0118f5afa
Clzzi <clzzi1109@naver.com>
2022-04-15 14:05:05 +0900

    chore(hooks): adding useForm custom Hooks

---
+++ src/core/hooks/useForm.ts
@@ -0,0 +1,43 @@
+import { FormEvent, useEffect, useState } from 'react';
+
+export type Error<T> = { [P in keyof T]?: string };
+
+interface Options<T> {
+  initialValue: T;
+  onSubmit(values: T): void;
+  validate(values: T): Error<T>;
+}
+
+export const useForm = <T, H extends HTMLElement = HTMLFormElement>({
+  initialValue,
+  onSubmit,
+  validate,
+}: Options<T>) => {
+  const [errors, setErrors] = useState<Error<T>>({});
+  const [values, setValues] = useState<T>(initialValue);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const newError: Error<T> = validate(values);
+    setErrors(newError);
+  }, [validate, values]);
+
+  const handleSubmit = async (e: FormEvent<H>): Promise<void> => {
+    setIsLoading(true);
+    e.preventDefault();
+    const newErrors: Error<T> | {} = validate ? validate(values) : {};
+    if (Object.keys(newErrors).length === 0) {
+      await onSubmit(values);
+    }
+    setErrors(newErrors);
+    setIsLoading(false);
+  };
+
+  return {
+    values,
+    errors,
+    isLoading,
+    setValues,
+    handleSubmit,
+  };
+};
