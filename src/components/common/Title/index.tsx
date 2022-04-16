b777075f33d055c3797f6aa45d7f96d7e20dd3b7
Clzzi <clzzi1109@naver.com>
2022-04-16 15:53:50 +0900

    chore(login): adding login-title

--- src/components/common/Title/index.tsx
+++ src/components/common/Title/index.tsx
@@ -1,32 +1,69 @@
+import { CSSProperties } from 'react';
 import styled from 'styled-components';
+import * as M from 'src/core/styles/styleMoudle';
 
 interface Props {
-  titleSize?: string;
-  descSize?: string;
+  mainSize?: string;
+  subSize?: string;
   width?: string;
   height?: string;
+  mainColor?: string;
+  subColor?: string;
+  mainText: string;
+  subText: string;
 }
 
-export const Title = () => {
+export const Title = ({
+  height = '78px',
+  mainColor = '#ffffff',
+  subColor = '#c4c4c4',
+  mainSize = '28px',
+  subSize = '16px',
+  width = '488px',
+  mainText,
+  subText,
+}: Props) => {
+  const mainStyle: CSSProperties = {
+    color: mainColor,
+    fontSize: mainSize,
+  };
+  const subStyle: CSSProperties = {
+    color: subColor,
+    fontSize: subSize,
+  };
+  const wrappperStyle: CSSProperties = {
+    width,
+    height,
+  };
+
   return (
-    <Wrapper>
-      <Content></Content>
+    <Wrapper style={wrappperStyle}>
+      <MainText style={mainStyle}>{mainText}</MainText>
       <Line />
-      <Desc></Desc>
+      <SubText style={subStyle}>{subText}</SubText>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
   display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: center;
-  text-align: start;
+  flex-direction: column;
+  align-items: flex-start;
+  text-align: center;
 `;
 
-const Content = styled.span``;
+const MainText = styled.span`
+  font-weight: bold;
+`;
 
-const Desc = styled.span``;
+const SubText = styled.span`
+  font-weight: normal;
+`;
 
-const Line = styled.div``;
+const Line = styled.div`
+  animation: ${M.ShowLeftToRight} 1s ease-out;
+  margin: 4px 0px 12px 0px;
+  background-color: ${({ theme }) => theme.colors.Main1};
+  width: 100%;
+  height: 2px;
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,44 @@
 import type { NextPage } from 'next';
 import { Header } from 'src/components/common/Layout/Header';
+import { Title } from 'src/components/common/Title';
+import styled, { useTheme } from 'styled-components';
 
-const Home: NextPage = () => {
-  return <div>j</div>;
+const Login: NextPage = () => {
+  const theme = useTheme();
+  return (
+    <Wrapper>
+      <Container>
+        <Title
+          mainSize="28px"
+          subSize="16px"
+          width="488px"
+          height="76px"
+          mainColor={theme.colors.White900}
+          subColor={theme.colors.Gray500}
+          mainText="로그인"
+          subText="로그인하여 더 자세한 정보를 찾아봐요!"
+        />
+      </Container>
+    </Wrapper>
+  );
 };
 
-export default Home;
+const Wrapper = styled.section`
+  width: 100%;
+  height: calc(100vh - 56px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
+
+const Container = styled.article`
+  width: 580px;
+  height: 560px;
+  padding: 36px;
+  display: flex;
+  background-color: ${({ theme }) => theme.colors.Black900};
+  border-radius: 12px;
+`;
+
+export default Login;
