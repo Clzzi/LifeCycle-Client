19018656938f39a4bfb609601aed731479e4c15e
Clzzi <clzzi1109@naver.com>
2022-05-11 12:17:34 +0900

    feat(handler): addign axios response, request, error handler

--- src/components/common/Layout/Layout.tsx
+++ src/components/common/Layout/Layout.tsx
@@ -1,8 +1,10 @@
 import { Header } from './Header';
 import { NextRouter, useRouter } from 'next/router';
 import { ReactNode, useEffect, useState } from 'react';
+import { useAxiosInterceptor } from 'src/core/hooks/useAxiosInterceptor';
 
 export const Layout = ({ children }: { children: ReactNode }) => {
+  useAxiosInterceptor();
   const router: NextRouter = useRouter();
   const [visible, setVisible] = useState<boolean>(false);
 
---
+++ src/core/hooks/useAxiosInterceptor.ts
@@ -0,0 +1,68 @@
+import {
+  ACCESS_TOKEN_KEY,
+  REFRESH_TOKEN_KEY,
+  TOKEN_HEADER_KEY,
+} from 'src/core/constants/api.constants';
+import { useEffect } from 'react';
+import { useToast } from './useToast';
+import { AxiosRequestConfig, AxiosResponse } from 'axios';
+import customAxios from '../libs/axios/customAxios';
+import TokenUtil from 'src/core/utils/token';
+import Auth from 'src/core/apis/auth/auth.api';
+import { Response } from 'src/types/common.type';
+
+export const useAxiosInterceptor = () => {
+  const { fireToast } = useToast();
+
+  const errorHandler = (error: Response): void => {
+    fireToast({ content: ` ${error.message} 🔥 ` });
+  };
+
+  const responseHandler = (response: AxiosResponse): AxiosResponse => {
+    return response;
+  };
+
+  const requestHandler = async (
+    config: AxiosRequestConfig,
+  ): Promise<AxiosRequestConfig> => {
+    let accessToken: string = TokenUtil.get(ACCESS_TOKEN_KEY);
+    let usingRefreshToken: string = TokenUtil.get(REFRESH_TOKEN_KEY);
+
+    if (accessToken && usingRefreshToken) {
+      const decode: any = TokenUtil.decode(ACCESS_TOKEN_KEY);
+      const nowDate: number = Date.now() / 1000;
+
+      if (decode.exp < nowDate) {
+        try {
+          const data: any = await Auth.tokenReissuance({
+            refreshToken: usingRefreshToken,
+          });
+          TokenUtil.set(ACCESS_TOKEN_KEY, data.data);
+          accessToken = data.data;
+        } catch (e: any) {
+          if (e.response.data.status === 410) {
+            TokenUtil.remove();
+          }
+        }
+      }
+      config.headers![TOKEN_HEADER_KEY] = accessToken;
+    }
+
+    return config;
+  };
+
+  const responseInterceptor: number = customAxios.interceptors.response.use(
+    (response: AxiosResponse) => responseHandler(response),
+    (error) => errorHandler(error.response.data),
+  );
+
+  const requestInterceptor: number =
+    customAxios.interceptors.request.use(requestHandler);
+
+  useEffect(() => {
+    return () => {
+      customAxios.interceptors.request.eject(requestInterceptor);
+      customAxios.interceptors.response.eject(responseInterceptor);
+    };
+  }, [responseInterceptor, requestInterceptor]);
+};
