8261b238c0436d16665a8c53171007641a1a20d6
Clzzi <clzzi1109@naver.com>
2022-05-12 16:12:59 +0900

    feat(scripts): fix build scripts

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: false,
+  serverRuntimeConfig: {
+    PROJECT_ROOT: __dirname,
+  },
   assetPrefix:
     process.env.NEXT_PUBLIC_NODE_ENV === 'production'
       ? 'http://lifecycle.kro.kr'
