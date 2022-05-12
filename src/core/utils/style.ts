09c532c2f6760e18bcded829de4b52e6d1412053
Clzzi <clzzi1109@naver.com>
2022-05-13 08:20:35 +0900

    chore(profile): change profile url

--- src/core/utils/style.ts
+++ src/core/utils/style.ts
@@ -20,22 +20,22 @@ export const handleTagColor = (
 export const handleProfileImg = (generation: number): string => {
   switch (generation) {
     case 0:
-      return 'url("/assets/unsigned-profile.svg")';
+      return 'url("http://lifecycle-s3.s3.ap-northeast-2.amazonaws.com/assets/unsigned-profile.svg")';
     case 1:
-      return 'url("/assets/Koala.svg")';
+      return 'url("http://lifecycle-s3.s3.ap-northeast-2.amazonaws.com/assets/Koala.svg")';
     case 2:
-      return 'url("/assets/Chicken.svg")';
+      return 'url("http://lifecycle-s3.s3.ap-northeast-2.amazonaws.com/assets/Chicken.svg")';
     case 3:
-      return 'url("/assets/Cow.svg")';
+      return 'url("http://lifecycle-s3.s3.ap-northeast-2.amazonaws.com/assets/Cow.svg")';
     case 4:
-      return 'url("/assets/Rabbit.svg")';
+      return 'url("http://lifecycle-s3.s3.ap-northeast-2.amazonaws.com/assets/Rabbit.svg")';
     case 5:
-      return 'url("/assets/Fox.svg")';
+      return 'url("http://lifecycle-s3.s3.ap-northeast-2.amazonaws.com/assets/Fox.svg")';
     case 6:
-      return 'url("/assets/Lion.svg")';
+      return 'url("http://lifecycle-s3.s3.ap-northeast-2.amazonaws.com/assets/Lion.svg")';
     case 7:
-      return 'url("/assets/Tiger.svg")';
+      return 'url("http://lifecycle-s3.s3.ap-northeast-2.amazonaws.com/assets/Tiger.svg")';
     default:
-      return 'url("/assets/Bear.svg")';
+      return 'url("http://lifecycle-s3.s3.ap-northeast-2.amazonaws.com/assets/Bear.svg")';
   }
 };
