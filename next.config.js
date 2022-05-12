de039903a8daad22acda21d3b29b5c3b6966ee9b
Clzzi <clzzi1109@naver.com>
2022-05-13 08:14:08 +0900

    chore(build): fix scripts in config

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: false,
-  serverRuntimeConfig: {
-    PROJECT_ROOT: __dirname,
-  },
   assetPrefix:
     process.env.NEXT_PUBLIC_NODE_ENV === 'production'
       ? 'http://lifecycle.kro.kr'
