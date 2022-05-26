1df1b0be2d06e4c1320d62c6a1aefa8464e987bd
Clzzi <clzzi1109@naver.com>
2022-05-26 16:14:47 +0900

    refactor(document): document file, favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,19 @@
-import { Html, Main, NextScript } from 'next/document';
+import { Html, Main, NextScript, Head } from 'next/document';
 
 function Document() {
   return (
     <Html lang="en">
+      <Head>
+        <link
+          rel="preload"
+          as="style"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
